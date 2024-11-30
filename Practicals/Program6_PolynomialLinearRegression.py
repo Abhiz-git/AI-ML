828b838d8e68b1b70760d6188e4177ed7f2056ea
Abhiz-git <abhisheknale.cs+github@gmail.com>
2024-12-01 01:45:27 +0530

    Complete Polynomial_Regression code

--- Practicals/Program6_PolynomialLinearRegression.py
+++ Practicals/Program6_PolynomialLinearRegression.py
@@ -3,16 +3,17 @@
 #   Name: Abhishek Dilipkumar Nale
 #   
 #   Original:   
-#   Date: 30/11/2024
+#   Date: 31/11/2024
 #
 #   Question 6: Write a python program to implement Polynomial Regression for predicting Salary.
 #
 ######################################################################################################
 
-
 import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
 
 
 def Main():
@@ -21,8 +22,47 @@ def Main():
     # reading data
     data = pd.read_csv(r'..\csv\Salary_Data.csv')
 
-    print()
+    # print(data.shape)
     print(data.info())
+
+    x = data.iloc[:, 0:1].values # 2D array
+    y = data.iloc[:, 1].values # 1D array
+
+    plt.scatter(x,y)
+    plt.plot(x,y)
+    plt.show()
+
+    slr= LinearRegression().fit(x,y)
+    slr_pred = slr.predict(x)
+
+    print("Acuracy of Slr is {:.2f}%\n".format(slr.score(x,y)*100))
+
+#   Plot Display to decide degree
+    # for i in range(5,13):
+            
+    #     x1 = PolynomialFeatures(degree=i).fit_transform(x)
+    #     plr = LinearRegression().fit(x1,y)
+
+    #     plt.scatter(x,y)
+        
+    #     plt.plot(x,plr.predict(x1))
+    #     plt.show()
+
+
+    x1 = PolynomialFeatures(degree=12).fit_transform(x)
+    plr = LinearRegression().fit(x1,y)
+
+    plt.scatter(x,y)
+        
+    plt.plot(x,plr.predict(x1))
+    plt.show()
+
+    plr_pred = plr.predict(x1)
+
+    print("Acuracy of Plr is {:.2f}%\n".format(plr.score(x1, y)*100))
+    
+
+
 
 
 
